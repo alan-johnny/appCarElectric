768316114e5af268e72f70baa5ad84c1c29199dc
Alan Johnny <johnnypontes711@gmail.com>
2024-08-05 17:44:24 -0300

    implement metodo

--- app/src/main/java/com/example/eletriccarapp/presentation/CalcularAutonomiaActivity.kt
+++ app/src/main/java/com/example/eletriccarapp/presentation/CalcularAutonomiaActivity.kt
@@ -1,12 +1,39 @@
 package com.example.eletriccarapp.presentation
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.eletriccarapp.R
 
 class CalcularAutonomiaActivity : AppCompatActivity() {
+    lateinit var preco: EditText
+    lateinit var kmPercorridos: EditText
+    lateinit var resultado: TextView
+    lateinit var bntCalcular: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calcular_autonomia)
+        setupViews()
+    }
+
+    fun setupViews() {
+        preco = findViewById(R.id.et_preco_kWh)
+        kmPercorridos = findViewById(R.id.et_km_value)
+        resultado = findViewById(R.id.tv_result)
+        bntCalcular = findViewById(R.id.btn_calcular)
+    }
+
+    fun setupListeners() {
+        bntCalcular.setOnClickListener {
+            calcular()  // call the function to calculate the autonomy.
+        }
+
+    fun calcular() {
+        val preco= preco.text.toString().toFloat()
+        val km = kmPercorridos.text.toString().toFloat()
+        val result =  preco / km
+        resultado.text = result.toString()
     }
 }
--- app/src/main/java/com/example/eletriccarapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/eletriccarapp/presentation/MainActivity.kt
@@ -9,10 +9,9 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.eletriccarapp.R
 
 class MainActivity : AppCompatActivity() {
-    lateinit var preco: EditText
-    lateinit var kmPercorridos: EditText
+
     lateinit var bntCalcular: Button
-    lateinit var resultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,27 +22,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupViews() {
-        preco = findViewById(R.id.et_preco_kWh)
-        kmPercorridos = findViewById(R.id.et_km_value)
+
         bntCalcular = findViewById(R.id.btn_cacular)
-        resultado = findViewById(R.id.tv_result)
+
     }
 
     fun setupListeners() {
         bntCalcular.setOnClickListener {
-           //calcular()
+
             startActivity(Intent(this,CalcularAutonomiaActivity::class.java))
         }
 
     }
 
 
-    fun calcular() {
-        val preco= preco.text.toString().toFloat()
-        val km = kmPercorridos.text.toString().toFloat()
-        val result =  preco / km
-        resultado.text = result.toString()
-    }
+
 
 
 }
