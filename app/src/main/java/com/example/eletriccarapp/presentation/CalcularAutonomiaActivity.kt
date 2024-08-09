ad9af44a002098644982764841ed7c70ae453d51
Alan Johnny <johnnypontes711@gmail.com>
2024-08-09 15:38:07 -0300

    v1.2

--- app/src/main/java/com/example/eletriccarapp/presentation/CalcularAutonomiaActivity.kt
+++ app/src/main/java/com/example/eletriccarapp/presentation/CalcularAutonomiaActivity.kt
@@ -3,19 +3,24 @@ package com.example.eletriccarapp.presentation
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.eletriccarapp.R
 
 class CalcularAutonomiaActivity : AppCompatActivity() {
+
     lateinit var preco: EditText
     lateinit var kmPercorridos: EditText
     lateinit var resultado: TextView
     lateinit var bntCalcular: Button
+    lateinit var bntClose: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calcular_autonomia)
         setupViews()
+        setupListeners()
     }
 
     fun setupViews() {
@@ -23,12 +28,21 @@ class CalcularAutonomiaActivity : AppCompatActivity() {
         kmPercorridos = findViewById(R.id.et_km_value)
         resultado = findViewById(R.id.tv_result)
         bntCalcular = findViewById(R.id.btn_calcular)
+        bntClose = findViewById(R.id.ic_close)
+
     }
 
     fun setupListeners() {
+
         bntCalcular.setOnClickListener {
             calcular()  // call the function to calculate the autonomy.
         }
+
+        bntClose.setOnClickListener {
+            finish()
+        }
+    }
+
 
     fun calcular() {
         val preco= preco.text.toString().toFloat()
--- app/src/main/java/com/example/eletriccarapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/eletriccarapp/presentation/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.eletriccarapp.presentation
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.eletriccarapp.R
@@ -11,20 +13,26 @@ import com.example.eletriccarapp.R
 class MainActivity : AppCompatActivity() {
 
     lateinit var bntCalcular: Button
+    lateinit var lista : ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupViews()
-
-
         setupListeners()
+        setupList()
     }
 
     fun setupViews() {
 
         bntCalcular = findViewById(R.id.btn_cacular)
+        lista = findViewById(R.id.lv_informacoes)
 
+    }
+    fun setupList() {
+        val dados = arrayOf("watter","sugar", "coffee", "milk")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, dados)
+        lista.adapter = adapter
     }
 
     fun setupListeners() {
