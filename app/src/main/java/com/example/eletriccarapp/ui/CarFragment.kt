070fe2a8ff97c59e8df8dd607b7eedd57c239b70
Alan Johnny <johnnypontes711@gmail.com>
2024-08-20 09:02:21 -0300

    floatButton

--- app/src/main/java/com/example/eletriccarapp/ui/CarFragment.kt
+++ app/src/main/java/com/example/eletriccarapp/ui/CarFragment.kt
@@ -1,21 +1,32 @@
 package com.example.eletriccarapp.ui
 
 import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eletriccarapp.R
 import com.example.eletriccarapp.data.CarFactory
+import com.example.eletriccarapp.domain.Carro
 import com.example.eletriccarapp.ui.adapter.CarAdapter
+import org.json.JSONArray
+import org.json.JSONObject
+import org.json.JSONTokener
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class CarFragment: Fragment() {
 
 
     lateinit var lista : RecyclerView
+    var carrosArray : ArrayList<Carro> = ArrayList()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,8 +36,9 @@ class CarFragment: Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        callService()
         setupView(view)
-        setupList()
+
     }
 
     fun setupView(view: View) {
@@ -34,8 +46,84 @@ class CarFragment: Fragment() {
         lista = view.findViewById(R.id.rv_list_car)
     }
     fun setupList() {
-        val adapter = CarAdapter(CarFactory.list)
+        val adapter = CarAdapter(carrosArray)
         lista.adapter = adapter
     }
+    fun callService(){
+        val urlBase = "https://igorbag.github.io/cars-api/cars.json"
+        MyTask().execute(urlBase)
+    }
 
+    inner class MyTask: AsyncTask<String, String, String>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+
+        }
+        override fun doInBackground(vararg urls: String?): String {
+          var urlConnection : HttpURLConnection? = null
+            try {
+                val urlbase = URL(urls[0])
+                urlConnection = urlbase.openConnection() as HttpURLConnection
+                urlConnection.connectTimeout = 60000
+                urlConnection.readTimeout = 60000
+                urlConnection.setRequestProperty("accept","application/json")
+
+                val responseCode = urlConnection.responseCode
+
+                if (responseCode == HttpURLConnection.HTTP_OK){
+
+                val response = urlConnection.inputStream.bufferedReader().use { it.readText() }
+                publishProgress(response)
+                }else{
+                    Log.e("Erro", "Erro ao buscar dados da API")
+                }
+            }catch(ex : Exception) {
+                Log.e("Erro", "Erro ao conectar com a API")
+            }finally {
+                urlConnection?.disconnect()
+            }
+            return ""
+        }
+        override fun onProgressUpdate(vararg values: String?) {
+            try {
+                val jsonArray = JSONTokener(values[0]).nextValue() as JSONArray
+                for (i in 0 until jsonArray.length()) {
+
+                    val id = jsonArray.getJSONObject(i).getString("id")
+                    Log.d("ID ->", id)
+
+                    val preco = jsonArray.getJSONObject(i).getString("preco")
+                    Log.d("Preco ->", preco)
+
+                    val bateria = jsonArray.getJSONObject(i).getString("bateria")
+                    Log.d("Bateria ->", bateria)
+
+                    val potencia = jsonArray.getJSONObject(i).getString("potencia")
+                    Log.d("Potencia ->", potencia)
+
+                    val recarga = jsonArray.getJSONObject(i).getString("recarga")
+                    Log.d("Recarga ->", recarga)
+
+                    val urlPhoto = jsonArray.getJSONObject(i).getString("urlPhoto")
+                    Log.d("urlPhoto ->", urlPhoto)
+
+                    val model = Carro(
+                        id = id.toInt(),
+                        preco = preco,
+                        bateria = bateria,
+                        potencia = potencia,
+                        recarga = recarga,
+                        urlPhoto = urlPhoto
+                    )
+                    carrosArray.add(model)
+                }
+                setupList()
+
+            }catch(ex : Exception) {
+                Log.e("Erro", "Erro ao processar dados")
+            }
+        }
+
+
+    }
 }
